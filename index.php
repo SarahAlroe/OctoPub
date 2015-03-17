549b760159f3761e5698fb34089879c567c6c0ac
root <silasalroe@gmail.com>
2015-03-17 18:05:23 +0100

    Added some stuff

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>OctoPub</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<img src="Logo.png" class="shadow logo">
+<h1>OctoPub</h1>
+<br>
+<div class="Container">
+    <div class="item shadow">
+        <h2>Login</h2>
+        <form>
+            <input type="text" name="name" class="textinput"><br>
+            <input type="submit" value="Login" class="button">
+        </form>
+    </div>
+</div>
+</body>
+</html>
