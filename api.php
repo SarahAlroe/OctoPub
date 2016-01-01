8dd1dbf3e4203e3c8677bb7530eef2a37700cbff
Silas Alroe <silasalroe@gmail.com>
2016-01-01 05:48:30 +0100

    Id requests are now limited to 50 with a reset time of 5 min.

--- api.php
+++ api.php
@@ -179,14 +179,27 @@ function returnID($id)
 {
     //Hash and return id and secId.
     global $r;
+    //Limit the user to 50 id requests in a 5min interval.
+    $userHash = "id_".hash("md4", $_SERVER['REMOTE_ADDR']);
+    $r->select(2);
+    if ($r->exists($userHash)) {
+        $idRequests = $r->get($userHash);
+        $idRequests += 1;
+        $r->set($userHash, $idRequests);
+        $r->expire($userHash, 300);
+        if ($idRequests > 50) {
+            return "ERR: Too many requests for a new id.";
+        }
+    } else {
+        $r->set($userHash, 1);
+        $r->expire($userHash, 300);
+    }
     $r->select(1);
     //Get salty
     $salt = $r->get("salt");
     //Generate id hash
     $secId = hash("sha256", $id . $salt);
     //Return array
-    //Sleep to delay brute force attempts.
-    sleep(1);
     return array($id, $secId);
 }
 
